13ec5123137599c4788f3b2474b6cabb8149c67a
Yoshifumi-T <2g.4423@gmail.com>
2020-05-14 23:26:29 +0900

    文字を円形配置、回転

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 'usestrict'
 
+function deployCharCircle() {
+    const angle = 5.3;
+    const target = document.querySelector('#js-text-area');
+    const str = "I thought what I'd do was, I'd pretend I was one of those deaf-mutes";
+    const chars = Array.from(str);
+    
+    chars.forEach(function(c, i, arr) {
+        let span = document.createElement('span');
+        span.innerHTML = c;
+        span.style.display = 'inline-block';
+        span.style.position = 'absolute';
+        span.style.left = '50%';
+        span.style.top = '50%';
+        span.style.transformOrigin = 'left top';
+        span.style.transform =
+            `rotate(${(i * angle - angle * ((arr.length / 2) >> 0))}deg)` +
+            'translate(0%, 40%)' +
+            `translate(0, -${(target.offsetWidth * 0.5)}px)` +
+            'translate(-50%, -50%)';
+        target.appendChild(span);
+    });
+}
 
+deployCharCircle();
 
